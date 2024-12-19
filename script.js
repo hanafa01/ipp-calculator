635620db2257e154e0d9c221e7076e2f44f4a13d
Hana <hana_fakhouri@hotmail.com>
2024-12-20 00:45:59 +0200

    dark mode design and functionality

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ toggleMenu.addEventListener("click", () => {
   sideMenu.classList.toggle("active");
 });
 //end sidebar
+
+
+// Dark Mode
+var checkbox = document.querySelector("input[name=theme]");
+
+checkbox.addEventListener("change", function () {
+  trans();
+  if (this.checked) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
+  }
+});
+
+function trans() {
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+}
+// End Dark Mode
